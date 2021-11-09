d673514f6b69ce4688a7d5f684e54d0ce1c9f86f
Jonas Wolff <jonas.wolff@tallence.com>
2021-11-09 15:59:30 +0100

    fix unitTests of TestApp & adjust Presentation

--- app/src/main/java/com/tallence/exampleApp/TestApplication.java
+++ app/src/main/java/com/tallence/exampleApp/TestApplication.java
@@ -1,6 +1,7 @@
 package com.tallence.exampleApp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
---
+++ app/src/test/java/com/tallence/advancedbdd/TestApplicationTests.java
@@ -0,0 +1,15 @@
+package com.tallence.advancedbdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@SpringBootConfiguration
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
