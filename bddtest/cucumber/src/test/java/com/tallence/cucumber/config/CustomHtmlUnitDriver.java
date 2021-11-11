072e0e099a2caac4c3a0cb741cbc8b3a19fc2f34
Jonas Wolff <jonas.wolff@tallence.com>
2021-11-11 08:43:39 +0100

    CustomHtmlUnitDriver

---
+++ bddtest/cucumber/src/test/java/com/tallence/cucumber/config/CustomHtmlUnitDriver.java
@@ -0,0 +1,18 @@
+package com.tallence.cucumber.config;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class CustomHtmlUnitDriver extends HtmlUnitDriver {
+    @Override
+    protected WebClient modifyWebClient(WebClient client) {
+        log.info("uses modified htmlunit driver");
+        WebClient modifiedClient = super.modifyWebClient(client);
+        modifiedClient.getOptions().setThrowExceptionOnScriptError(false); // see here
+        return modifiedClient;
+    }
+}
